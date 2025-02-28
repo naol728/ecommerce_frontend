72689f9142af705543bff3712da2a7227d37de13
naol <naolmeseret22@gmail.com>
2025-02-28 13:15:16 +0300

    adding cards for the product

--- src/pages/Carts.jsx
+++ src/pages/Carts.jsx
@@ -21,33 +21,12 @@ export default function Carts() {
   };
 
   const DrawerList = (
-    <Box sx={{ width: 250 }} role="presentation" onClick={toggleDrawer(false)}>
-      <List>
-        {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
-          <ListItem key={text} disablePadding>
-            <ListItemButton>
-              <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-              </ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItemButton>
-          </ListItem>
-        ))}
-      </List>
-      <Divider />
-      <List>
-        {["All mail", "Trash", "Spam"].map((text, index) => (
-          <ListItem key={text} disablePadding>
-            <ListItemButton>
-              <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-              </ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItemButton>
-          </ListItem>
-        ))}
-      </List>
-    </Box>
+    <Box
+      sx={{ width: 400 }}
+      anchor="right"
+      role="presentation"
+      onClick={toggleDrawer(false)}
+    ></Box>
   );
 
   return (
@@ -57,7 +36,7 @@ export default function Carts() {
           <ShoppingCartIcon fontSize="medium" />
         </Badge>
       </IconButton>
-      <Drawer open={open} onClose={toggleDrawer(false)}>
+      <Drawer open={open} anchor="right" onClose={toggleDrawer(false)}>
         {DrawerList}
       </Drawer>
     </div>
