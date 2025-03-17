47c676314477daa68914a3444a955b1314db942d
naol <naolmeseret22@gmail.com>
2025-03-17 20:08:10 +0300

    done with adding product to the api

--- src/components/custom/DashboardSidebar.jsx
+++ src/components/custom/DashboardSidebar.jsx
@@ -1,5 +1,18 @@
-import React from "react";
-import { Drawer, List, ListItem, ListItemIcon, ListItemText, Toolbar, Divider } from "@mui/material";
+import React, { useState } from "react";
+import {
+  Drawer,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  Toolbar,
+  Divider,
+  IconButton,
+  AppBar,
+  Box,
+  CssBaseline,
+} from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
 import DashboardIcon from "@mui/icons-material/Dashboard";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 import AddBoxIcon from "@mui/icons-material/AddBox";
@@ -10,18 +23,14 @@ import { Link } from "react-router-dom";
 const drawerWidth = 240;
 
 const DashboardSidebar = () => {
-  return (
-    <Drawer
-      variant="permanent"
-      sx={{
-        width: drawerWidth,
-        flexShrink: 0,
-        "& .MuiDrawer-paper": {
-          width: drawerWidth,
-          boxSizing: "border-box",
-        },
-      }}
-    >
+  const [mobileOpen, setMobileOpen] = useState(false);
+   
+  const handleDrawerToggle = () => {
+    setMobileOpen(!mobileOpen);
+  };
+
+  const drawerContent = (
+    <div>
       <Toolbar />
       <Divider />
       <List>
@@ -62,7 +71,58 @@ const DashboardSidebar = () => {
           <ListItemText primary="Logout" />
         </ListItem>
       </List>
-    </Drawer>
+    </div>
+  );
+
+  return (
+    <Box sx={{ display: "flex" }}>
+      <CssBaseline />
+
+      {/* App Bar with Hamburger Button */}
+      <AppBar position="fixed" sx={{ zIndex: 1201, background: "#000" }}>
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            edge="start"
+            onClick={handleDrawerToggle}
+            sx={{ mr: 2, display: { sm: "none" } }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <h3>NAF Brands</h3>
+        </Toolbar>
+      </AppBar>
+
+      {/* Permanent Sidebar on Large Screens */}
+      <Drawer
+        variant="permanent"
+        sx={{
+          width: drawerWidth,
+          flexShrink: 0,
+          display: { xs: "none", sm: "block" }, // Hide on small screens
+          "& .MuiDrawer-paper": {
+            width: drawerWidth,
+            boxSizing: "border-box",
+          },
+        }}
+      >
+        {drawerContent}
+      </Drawer>
+
+      {/* Temporary Sidebar for Small Screens (Hamburger Menu) */}
+      <Drawer
+        variant="temporary"
+        open={mobileOpen}
+        onClose={handleDrawerToggle}
+        ModalProps={{ keepMounted: true }}
+        sx={{
+          display: { xs: "block", sm: "none" },
+          "& .MuiDrawer-paper": { width: drawerWidth },
+        }}
+      >
+        {drawerContent}
+      </Drawer>
+    </Box>
   );
 };
 
